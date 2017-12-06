e548a6e05b9934fb17ed38c5297630852397a109
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2017-12-06 13:35:56 -0700

    finshed

--- assests/JS/cargif.js
+++ assests/JS/cargif.js
@@ -1,39 +1,55 @@
 $(document).ready(function(){
 
 var topics =[
-	'Audi', 
-	'AstonMartin',
-	'Tesla',
-	'Nissan',
+	'Audi R8', 
+	'Porsche 911',
+	'Nissan GTR',
+	'Mazda Miata',
+	'Toyota Supra',
 	'Lexus',
-	'Saab'
+	'Tesla'
 ];
 //geting gifs from ajax 
 function displayGif(){
 	var car = $(this).attr("data-name");
-	var queryURL ="https://api.giphy.com/v1/gifs/search?q=&api_key=uZGm777wAQsj1NHha0667cF4gwkvc9NO&q=" + car + "&limit=10&offset=0&rating=G&lang=en"	
+	var queryURL ="https://api.giphy.com/v1/gifs/search?q=&api_key=uZGm777wAQsj1NHha0667cF4gwkvc9NO&q=" + car + "&limit=10&offset=0&rating=pg&lang=en"	
 
 	$.ajax({
 		url:queryURL,
 		method: "GET"
 	}).done(function(response){
-		//geting responce form api
+		//geting response form api
 		console.log(response);	
+		//empting 
+		$("#gifs").empty();
 		//for loop to go thur topics to show all gifs 
 		for (var i = 0; i <response.data.length; i++){
-		//showing gifs in div  (AstonMartin only has one gif dont forget!)
-		var gifurl = response.data[i].images.fixed_height.url
-		var gif = $("<img>").attr("src", gifurl);
-		$("#gifs").append(gif); 	
-		//put response rating in div
-		var thediv = $("#gifs")
-		var rate = $("<p>").text('Rating: '+response.data[i].rating);
-		thediv.append(rate);
-		}
-	});
-}
-function makebutton(){
+		 	//showing gifs in div  (AstonMartin only has one gif dont forget!)
+			var gif = $("<img>").attr("src", stillSrc);
+        	var AnimatedSrc = response.data[i].images.fixed_height.url;
+        	var stillSrc = response.data[i].images.fixed_height_still.url;
 
+			gif.attr("data-state", "still")
+			gif.attr("data-still", stillSrc);
+			gif.attr("data-animate", AnimatedSrc);
+			gif.addClass("cargifs")
+			$("#gifs").append(gif); 	
+			 //put response rating in a p tag and appending it 
+			var thediv = $("#gifs")
+			var rate = $("<p>").text('Rating: '+response.data[i].rating);
+			thediv.append(rate);
+			//making the gifs play 
+		}	
+		});
+};
+
+
+
+
+
+
+
+function makebutton(){
 	$("#buttons").empty();
 //for loop thur array
 	for (var i = 0; i <topics.length;i++) {
@@ -45,11 +61,32 @@ function makebutton(){
 	$("#buttons").append(a);
 	}
 }
-makebutton();
-
-      $(document).on("click", ".topic", displayGif);
 //adding listener to all elements w/ class of topic
-     
+$(document).on("click", ".topic", displayGif);
+//creating a function to create movies 
+$("#carbutton").on("click", function(event){
+	event.preventDefault();
+	var addcar = $("#carinput").val().trim();
+	topics.push(addcar);
+	//calling make button from array function 
+	makebutton();
+	});
+//adding click event to gifs 
+  $(document).on("click", ".cargifs", pauseGifs);
+//function to call gifs
+function pauseGifs(){
+	  	 var state = $(this).attr("data-state");
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+  		}
+}
 
+
+//pulling up orignal array of cars 
+makebutton();
 // closeing ready 
 });
